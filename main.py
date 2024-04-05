f897ff952f5abfb51fd3af5a6cb71c2fd20f5ae4
Paulo Souza <souzapeus@gmail.com>
2024-04-05 19:50:56 -0300

    db

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import os
 
 
 # Conectar ao MongoDB
-#client = MongoClient('mongodb://localhost:27017/')
-#db = client['trainview']
-#usuarios_collection = db['trainview']
+client = MongoClient('mongodb://localhost:27017/')
+db = client['trainview']
+usuarios_collection = db['trainview']
 
 #app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 from app import app
