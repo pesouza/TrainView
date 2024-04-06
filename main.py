6ab0bafada69dc876d26a05edf96fdd80b7c8a4c
Paulo Souza <souzapeus@gmail.com>
2024-04-05 21:50:49 -0300

    index2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ usuarios_collection = db['users']
 from app import app
 
 # Caminho para o arquivo index.py
-INDEX_FILE = 'index.py'
+INDEX_FILE = '/var/www/TranView/index.py'
 
 # Criar modal de login
 login_modal = dbc.Modal(
