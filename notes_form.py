40de7b92de0ea50c57c86a79194583d94ad21543
Paulo Souza <souzapeus@gmail.com>
2024-04-15 15:01:35 -0300

    notes

--- notes_form.py
+++ notes_form.py
@@ -33,48 +33,14 @@ def update_notes(n_clicks, note, url, cut_scene):
 
     trigg = dash.callback_context.triggered[0]["prop_id"]
     if "btn-save-note.n_clicks" == trigg:
-        DICT_NOTES[key_name] = note
-        with open('dict_notes.json', 'w') as f:
-            json.dump(DICT_NOTES, f)
-        return DICT_NOTES[key_name]
+        # Salvar nota no MongoDB
+        notes_collection.update_one({"_id": key_name}, {"$set": {"note": note}}, upsert=True)
+        return note
         
     if ("video-player.url" in trigg or "dd-cut-scenes.value" in trigg) and key_name in DICT_NOTES.keys():
-        return DICT_NOTES[key_name]
-
-    
-
-# @app.callback(Output('intermediate-value', 'data'),
-#               [Input('btn-save-btn', 'n_clicks'), State("ipt-note", "value"),
-#               State("video-player", "url"), State("dd-cut-scenes", "value")])
-# def update_notes(n_clicks, note, url, cut_scene):
-#     cut_scene = "" if cut_scene is None else cut_scene
-#     url = "" if url is None else url
-
-#     key_name = url + "-" + cut_scene
-#     if key_name not in saved_note:
-#         saved_note[key_name] = []
-    
-#     saved_note[key_name] += [note]
-#     return saved_note
-
-
-# @app.callback(Output('div-notes-holder', 'children'),
-#               [Input('intermediate-value', 'data'),
-#               Input("video-player", "url"), Input("dd-cut-scenes", "value")])
-# def create_note_cards(saved_notes, url, cut_scene):
-#     cut_scene = "" if cut_scene is None else cut_scene
-#     url = "" if url is None else url
-#     key_name = url + "-" + cut_scene
-
-#     output_cards = []
-#     print(saved_notes)
-#     if key_name in saved_notes.keys():
-#         for i in saved_notes[key_name]:
-#             output_cards += [
-#                 dbc.Row([
-#                         dbc.Button("X", color="warning", className="mt-auto"),
-#                         dbc.Card(dbc.CardBody(i))
-#                     ])
-#                 ]
-
-#     return output_cards
+        # Recuperar nota do MongoDB
+        saved_note = notes_collection.find_one({"_id": key_name})
+        if saved_note:
+            return saved_note["note"]
+        else:
+            return ""
